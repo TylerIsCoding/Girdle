89e6fe52dbf76d632ef4b3fc33ae71bc920da6d2
Tyler McKenna <tmckenna1988@gmail.com>
2024-03-05 17:22:44 -0500

    Project mostly finished. Need to add 'timer' for the next challenge.

--- script.js
+++ script.js
@@ -15289,14 +15289,21 @@ const dictionary = [
     "rural",
     "shave",
 ];
+
 const guessGrid = document.querySelector("[data-guess-grid]");
+const alertContainer = document.querySelector("[data-alert-container]");
+const keyboard = document.querySelector("[data-keyboard]");
+const playerScore = Number(localStorage.getItem("score"));
 const WORD_LENGTH = 5;
+const FLIP_ANIMATION_DURATION = 500;
+const DANCE_ANIMATION_DURATION = 500;
 const offsetFromDate = new Date(2024, 2, 1);
 const msOffset = Date.now() - offsetFromDate;
 const dayOffset = msOffset / 1000 / 60 / 60 / 24;
 const targetWord = targetWords[Math.floor(dayOffset)];
 
 startInteraction();
+setScore(playerScore);
 
 function startInteraction() {
     document.addEventListener("click", handleMouseClick);
@@ -15306,6 +15313,14 @@ function startInteraction() {
 function stopInteraction() {
     document.removeEventListener("click", handleMouseClick);
     document.removeEventListener("keydown", handleKeyPress);
+}
+
+function setScore(playerScore, points) {
+    const scoreHolder = document.querySelector("[data-score]");
+    scoreHolder.innerText = playerScore || 0;
+    if (points) {
+        localStorage.setItem("score", Number(playerScore + points));
+    }
 }
 
 function handleMouseClick(e) {
@@ -15367,8 +15382,124 @@ function submitGuess() {
         shakeTiles(activeTiles);
         return;
     }
+
+    const guess = activeTiles.reduce((word, tile) => {
+        return word + tile.dataset.letter;
+    }, "");
+
+    if (!dictionary.includes(guess)) {
+        showAlert("Not in word list");
+        shakeTiles(activeTiles);
+        return;
+    }
+
+    stopInteraction();
+    activeTiles.forEach((...params) => {
+        flipTile(...params, guess);
+    });
+}
+
+function flipTile(tile, index, array, guess) {
+    const letter = tile.dataset.letter;
+    const key = keyboard.querySelector(`[data-key="${letter}"i]`);
+    setTimeout(() => {
+        tile.classList.add("flip");
+    }, (index * FLIP_ANIMATION_DURATION) / 2);
+    tile.addEventListener(
+        "transitionend",
+        () => {
+            tile.classList.remove("flip");
+            if (targetWord[index] === letter) {
+                tile.dataset.state = "correct";
+                key.classList.add("correct");
+            } else if (targetWord.includes(letter)) {
+                tile.dataset.state = "wrong-location";
+                key.classList.add("wrong-location");
+            } else {
+                tile.dataset.state = "wrong";
+                key.classList.add("wrong");
+            }
+
+            if (index === array.length - 1) {
+                tile.addEventListener(
+                    "transitionend",
+                    () => {
+                        startInteraction();
+                        checkWinLose(guess, array);
+                    },
+                    { once: true }
+                );
+            }
+        },
+        { once: true }
+    );
 }
 
 function getActiveTiles() {
     return guessGrid.querySelectorAll('[data-state="active"]');
+}
+
+function showAlert(message, duration = 1000) {
+    const alert = document.createElement("div");
+    alert.textContent = message;
+    alert.classList.add("alert");
+    alertContainer.prepend(alert);
+    if (duration == null) return;
+
+    setTimeout(() => {
+        alert.classList.add("hide");
+        alert.addEventListener("transitionend", () => {
+            alert.remove();
+        });
+    }, duration);
+}
+
+function shakeTiles(tiles) {
+    tiles.forEach((tile) => {
+        tile.classList.add("shake");
+        tile.addEventListener(
+            "animationend",
+            () => {
+                tile.classList.remove("shake");
+            },
+            { once: true }
+        );
+    });
+}
+
+function checkWinLose(guess, tiles) {
+    const remainingTiles = guessGrid.querySelectorAll(":not([data-letter])");
+
+    if (guess === targetWord) {
+        if (remainingTiles.length === 25) {
+            setScore(playerScore, 20);
+            showAlert("First Guess!!! +20 Points!", 5000);
+        } else {
+            setScore(playerScore, 10);
+            showAlert("You Win! +10 points!", 5000);
+        }
+        danceTiles(tiles);
+        stopInteraction();
+        return;
+    }
+
+    if (remainingTiles.length === 0) {
+        showAlert(`Today's word was: ${targetWord.toUpperCase()}`, null);
+        stopInteraction();
+    }
+}
+
+function danceTiles(tiles) {
+    tiles.forEach((tile, index) => {
+        setTimeout(() => {
+            tile.classList.add("dance");
+            tile.addEventListener(
+                "animationend",
+                () => {
+                    tile.classList.remove("dance");
+                },
+                { once: true }
+            );
+        }, (index * DANCE_ANIMATION_DURATION) / 5);
+    });
 }
